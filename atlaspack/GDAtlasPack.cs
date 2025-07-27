d3e7d34a9585518a477ecebb01cbdc1ad4e65730
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-07-28 00:41:26 +0800

    add atlas pack texture loading

--- atlaspack/GDAtlasPack.cs
+++ atlaspack/GDAtlasPack.cs
@@ -9,13 +9,29 @@ public partial class GDAtlasPack: Resource
 {
     [Export] public Array<GDAtlas> atlasList = new();
 
-    public void AddContext(AtlasContext context)
+    public void AddPack(AtlasPack pack)
     {
+        var context = pack.context;
+        var imageData = pack.image.imageData;
+        
         var atlas = new GDAtlas();
         atlas.dimension = new Vector2(context.bounds.width, context.bounds.height);
         
         if (atlasList == null)
             atlasList = new Array<GDAtlas>();
         atlasList.Add(atlas);
+        
+        var image = new Image();
+        var imageLoadError = image.LoadPngFromBuffer(imageData);
+        if (imageLoadError != Error.Ok)
+        {
+            GD.PushError($"Failed to load image from buffer: {imageLoadError}");
+            return;
+        }
+
+        var texture = ImageTexture.CreateFromImage(image);
+        var texturePath = $"res://.godot/atlas_cache/{GetName()}.png";
+        ResourceSaver.Save(texture, texturePath);
+        atlas.atlasImage = texture;
     }
 }
