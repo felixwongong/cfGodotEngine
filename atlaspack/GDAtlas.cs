bd2520da99e7f9e6643efca5e83cad6c009a70d2
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-07-28 15:41:21 +0800

    add GDAtlas imageMap

--- atlaspack/GDAtlas.cs
+++ atlaspack/GDAtlas.cs
@@ -1,5 +1,5 @@
-using System;
 using Godot;
+using Godot.Collections;
 
 namespace cfGodotEngine.Asset;
 
@@ -9,5 +9,6 @@ public partial class GDAtlas: Resource
 {
     [Export] public string atlasId;
     [Export] public Vector2 dimension;
+    [Export] public Dictionary<string, Rect2I> imageMap;
     [Export] public Texture2D atlasTexture;
 }
--- atlaspack/GDAtlasPack.cs
+++ atlaspack/GDAtlasPack.cs
@@ -14,6 +14,12 @@ public partial class GDAtlasPack: Resource
         var atlas = new GDAtlas();
         atlas.atlasId = atlasId;
         atlas.dimension = new Vector2(context.bounds.width, context.bounds.height);
+        var imageMap = new Dictionary<string, Rect2I>(); 
+        foreach (var (imageName, rect) in context.imageRectMap)
+        {
+            imageMap.Add(imageName, new Rect2I(rect.x, rect.y, rect.width, rect.height));
+        }
+        atlas.imageMap = imageMap;
         
         if (atlasList == null)
             atlasList = new Array<GDAtlas>();
