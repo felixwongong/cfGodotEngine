8fc3420a352f488fb4b85e09fa46f110d34d0027
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-09-02 00:11:04 +0800

    fix restore empty value & property check

--- binding/StringBinder.cs
+++ binding/StringBinder.cs
@@ -5,7 +5,7 @@ using Godot;
 namespace cfGodotEngine.Binding;
 
 /// <summary>
-/// Binder for binding to a <see cref="IPropertyMap"/> using <see cref="bindingName"/>
+/// Binder for binding to a <see cref="IPropertyMap"/> using <see cref="_bindingName"/>
 /// When property updated, signal <see cref="OnValueChanged"/> will be emitted, and <see cref="OnBindingValueChanged"/> will be called.
 /// For inherited member,
 /// 1. override the <see cref="OnBindingValueChanged"/> to add custom handling for the string value
@@ -19,7 +19,7 @@ public partial class StringBinder: Binder
 {
     [Signal] public delegate void OnValueChangedEventHandler(string value);
 
-    [Export] private BindingName bindingName;
+    [Export] private BindingName _bindingName;
 
     protected override void OnBindingRestored(IPropertyMap bindingMap)
     {
@@ -29,14 +29,20 @@ public partial class StringBinder: Binder
             return;
         }
 
-        bindingMap.Get(bindingName.propertyName, out object propertyValue);
-        var parsed = ParseValue(propertyValue);
-        OnPropertyChanged(parsed);
-        EmitSignalOnValueChanged(parsed);
+        bindingMap.Get(_bindingName.propertyName, out object propertyValue);
+        if (propertyValue != null)
+        {
+            var parsed = ParseValue(propertyValue);
+            OnPropertyChanged(parsed);
+            EmitSignalOnValueChanged(parsed);
+        }
     }
 
     protected override void OnBindingValueChanged(string propertyName, object propertyValue)
     {
+        if(!propertyName.Equals(_bindingName?.propertyName))
+            return;
+        
         var parsed = ParseValue(propertyValue);
         OnPropertyChanged(parsed);
         EmitSignalOnValueChanged(parsed);
